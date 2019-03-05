bca6c9e8112d747651a8d754998238e592dcdafa
Anitha Lavanya <anithalavanya6@gmail.com>
2019-03-05 09:56:38 +0530

    Update project.py

--- project.py
+++ project.py
@@ -163,9 +163,11 @@ def getUserInfo(user_id):
 
 
 def getUserID(email):
-
+ try:
     user = session.query(User).filter_by(email=email).one()
     return user.id
+ except:
+        return None
     # DISCONNECT - Revoke a current user's token and reset their login_session
 
 
